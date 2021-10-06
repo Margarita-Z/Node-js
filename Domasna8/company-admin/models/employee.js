11a7d55586a0b4cb46111fe46af1f83dc8a7ed8a
Margarita-Z <margaritazdraveve89@gmail.com>
2021-10-06 14:04:21 +0200

    domasna8

---
+++ Domasna8/company-admin/models/employee.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const employeeSchema = mongoose.Schema({
+    first_name: {
+        type: String,
+        require: true //zadolzitelno pole require
+    },
+    last_name: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: String,
+        require: true
+    },
+    salary: {
+        type: Number,
+        }
+});
+
+module.exports = mongoose.model('employee', employeeSchema);
---
+++ Domasna8/company-admin/routes/employees.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+const controller = require('../controllers/employees');
+
+router.get('/', controller.getAll)
+      .get('/create', controller.getCreate)
+      .get('/:id', controller.getUpdate)
+      .post('/', controller.postCreate)
+      .post('/:id',controller.postUpdate)
+
+module.exports = router;
