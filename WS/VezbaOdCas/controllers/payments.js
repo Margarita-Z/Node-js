ea148ac2498e92baa7523832dbfd4dc13366f5cd
Margarita-Z <margaritazdraveve89@gmail.com>
2021-11-10 23:49:18 +0100

    capture and refund transaction

--- WS/VezbaOdCas/controllers/payments.js
+++ WS/VezbaOdCas/controllers/payments.js
@@ -1,5 +1,7 @@
 const paypal = require('@paypal/checkout-server-sdk');
 const client = require('../utilities/paypal/client');
+const Payment = require('../models/payment');
+const Transaction = require('../models/transaction');
 
 module.exports = {
   create: async (req, res) => {
@@ -38,7 +40,7 @@ module.exports = {
          approve_link: approveLink
        })
   },
-  authorize: async (req, res) => {
+  /*authorize: async (req, res) => {
     const request = new paypal.orders.OrdersAuthorizeRequest(req.body.order_id);
     request.requestBody({});
     let response = await client.execute(request);
@@ -50,33 +52,111 @@ module.exports = {
        .send({
           authorization_id: response.result.purchase_units[0].payments.authorizations[0].id
        })
+  },*/
+
+  authorize: async (req, res) => {
+    const request = new paypal.orders.OrdersAuthorizeRequest(req.body.order_id);
+    request.requestBody({});
+    let response = await client.execute(request);
+
+    let authorization = response.result.purchase_units[0].payments.authorizations[0];
+
+    const payment = await Payment.create({
+      user: req.user.id,
+      status: 'authorized',
+      delivery_address: req.body.delivery_address
+    })
+
+    const transaction = await Transaction.create({
+      amount: authorization.amount.value,
+      payment: payment,
+      type: 'authorization',
+      external_reference: authorization.id,
+      processor_response: JSON.stringify(response.result)
+    })
+
+    console.log(request);
+    console.log(response);
+
+    res.status(response.statusCode)
+       .send({
+          transaction: transaction
+       })
+  },
+  void: async (req, res) => {
+    let transaction = await Transaction.findOne({ payment: req.params.id, type: 'authorization' });
+    const request = new paypal.payments.AuthorizationsVoidRequest(transaction.external_reference);
+    let response = await client.execute(request);
+
+    transaction = await Transaction.create({
+      amount: transaction.amount,
+      payment: transaction.payment,
+      type: 'void',
+      external_reference: transaction.external_reference,
+      processor_response: JSON.stringify(response)
+    })
+
+    await Payment.findByIdAndUpdate(req.params.id, { status: 'voided' });
+
+    console.log(request);
+    console.log(response);
+
+    res.status(response.statusCode)
+       .send({
+          transaction: transaction
+       })
   },
   capture: async (req, res) => {
-    request = new paypal.payments.AuthorizationsCaptureRequest(req.body.authorization_id);
+    let transaction = await Transaction.findOne({ payment: req.params.id, type: 'authorization' });
+    request = new paypal.payments.AuthorizationsCaptureRequest(transaction.external_reference);
     request.requestBody({});
     
     let response = await client.execute(request);
+    let capture =  response.result
+    console.log(response)
+
+
+    transaction = await Transaction.create({
+      amount: transaction.amount,
+      payment: transaction.payment,
+      type: 'capture',
+      external_reference: capture.id,
+      processor_response: JSON.stringify(response.result)
+    })
+    await Payment.findByIdAndUpdate(req.params.id, { status: 'capture' });
 
     console.log(request);
     console.log(response);
 
     res.status(response.statusCode)
     .send({
-        capture_id: response.result.id
+        transaction: transaction
     })
 
   },
   refund: async (req, res) => {
-    const request = new paypal.payments.CapturesRefundRequest(req.body.capture_id);
+    let transaction = await Transaction.findOne({ payment: req.params.id, type: 'capture' });
+    const request = new paypal.payments.CapturesRefundRequest(transaction.external_reference);
     request.requestBody({});
     console.log(request)
     
     let response = await client.execute(request);
+    let refund =  response.result
+
+
+    transaction = await Transaction.create({
+      amount: transaction.amount,
+      payment: transaction.payment,
+      type: 'refund',
+      external_reference: refund.id,
+      processor_response: JSON.stringify(response.result)
+    })
+    await Payment.findByIdAndUpdate(req.params.id, { status: 'refund' });
     console.log(request);
     console.log(response);
     res.status(response.statusCode)
     .send({
-        refund_id: response.result.id
-    })
+        transaction: transaction
+  })
   }
 };
